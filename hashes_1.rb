41a8992b9d63fa97a23155d6c0f2d6c505431855
Annie Pulzone <ajpulzone@gmail.com>
2022-07-15 20:28:20 -0700

    Update hash 1

--- hashes_1.rb
+++ hashes_1.rb
@@ -1,29 +1,36 @@
 
-Activity:
+#Activity:
 
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your Terminal: 
+# file by entering the following command in your Terminal:
 # `ruby hashes_1.rb`
 
-foods = {"apples": 23, "grapes": 507, "eggs": 48}
-p zoo
+foods = {
+  "apples": 23,
+  "grapes": 507,
+  "eggs": 48
+}
+#p zoo
 
-# Write code that prints all of the 'keys' of the foods variable 
+# Write code that prints all of the 'keys' of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.keys
 
-# Write code that prints all of the 'values' of the foods variable 
+# Write code that prints all of the 'values' of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.values
 
-# Write code that prints the value of the second food of the foods variable 
+# Write code that prints the value of the second food of the foods variable
 # you created above:
-# YOUR CODE HERE
+puts foods.values[1]
 
-# Write code that adds a food to the foods hash. 
+# Write code that adds a food to the foods hash.
 # Then, print the updated hash:
-# YOUR CODE HERE
+
+foods["carrots"] = 17
+
+puts foods
 
 
 #-------------------
@@ -31,22 +38,33 @@ p zoo
 #-------------------
 
 
-# Think about all the pieces of information associated with one single email in your inbox.
+# Think about all the pieces of information associated with one
+#single email in your inbox.
 # It has a sender, a subject, ...
 
-# Declare a variable that stores hash. Each key should be an attribute of an email and each
-# value should be some appropriate value for that key. Work to have at least 5 key-value pairs.
+# Declare a variable that stores hash. Each key should be an attribute of an
+# email and each value should be some appropriate value for that key.
+# Work to have at least 5 key-value pairs.
+
+emails = {
+  "to": "Tandy Newberry",
+  "from": "Fred Carbin",
+  "subject": "Work schedule for next week",
+  "attachments": 3,
+  "address": "happyday83@gmail.com"
+}
 
 # Write code that prints your email hash to the terminal.
+puts emails
 
 
-# Write code that prints all of the 'keys' of the email hash 
+# Write code that prints all of the 'keys' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts emails.keys
 
-# Write code that prints all of the 'values' of the email hash 
+# Write code that prints all of the 'values' of the email hash
 # you created above:
-# YOUR CODE HERE
+puts emails.values
 
 
 #-------------------
@@ -74,7 +92,7 @@ posts = [
     timestamp: "4:37 PM August 13, 2019",
     number_likes: 0,
     comments: []
-  }, 
+  },
   {
     image_src: "./images/holiday-party.png",
     caption: "What a great holiday party omg",
@@ -88,12 +106,39 @@ p posts
 p posts[0]
 
 
-# The code snippet above shows an Array with 2 elements. Each element in the Array is a 
-# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK 
+# The code snippet above shows an Array with 2 elements. Each element in the Array is a
+# Hash. Each of those Hashes has 4 key-value pairs. This may LOOK
 # a bit daunting - it's OK! You don't need to be 100% comfortable with this, but it's
 # good to have some exposure before going into Mod 1.
 
 
-# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same 
+# YOU DO: Create an array of at least 3 EMAIL Hashes, using the same
 # key-value pairs you used in your email Hash above.
 # Then, print the email Array to the Terminal.
+emails = ["Tandy", "Craig", "Sigfried"]
+#
+emails = [
+  {
+    to: "Tandy Newberry",
+    from: "Fred Carbin",
+    subject: "Work schedule for next week",
+    attachments: 3,
+    address: "happyday83@gmail.com"
+  },
+  {
+    to: "Craig Warson",
+    from: "Safrina Picton",
+    subject: "Holiday party pictures",
+    attachments: 19,
+    address: "goodthoughts13@yahoo.com"
+  },
+  {
+    to: "Sigfried Jensen",
+    from: "Fannie Wallowitz",
+    subject: "Vacation Request",
+    attachments: 10,
+    address: "fiddlesticks22@gmail.com"
+  }
+]
+p emails
+#p emails[3]
